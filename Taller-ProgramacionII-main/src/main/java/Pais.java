98b99e9d69431081045c52675527b5e58470554f
diegonat17 <diegonalarcont.17@gmail.com>
2026-06-22 20:43:51 -0300

    Update Pais.java

--- Taller-ProgramacionII-main/src/main/java/Pais.java
+++ Taller-ProgramacionII-main/src/main/java/Pais.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Pais {
     private String nombre;
     private String bandera;
     private ArrayList<Arbitro> arbitros;
-    // Lista de árbitros
+    private Seleccion seleccion;
+    private List<Sede> sedes;
+
+    public Pais(){ this("",""); }
+    
+    public Pais(String nombre, String bandera, Seleccion seleccion) {
+        this(nombre, bandera);
+        this.seleccion = seleccion;
+    }
 
     public Pais(String nombre, String bandera) {
-
-        this.arbitros = new ArrayList<Arbitro>();
         this.nombre = nombre;
         this.bandera = bandera;
+        this.arbitros = new ArrayList<Arbitro>();
+        this.sedes= new ArrayList<Sede>();
     }
 
     public String getBandera() { return bandera; }
@@ -18,11 +27,12 @@ public class Pais {
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
-    public List<Arbitro> getArbitros(){
-        return arbitros;
-    }
-    public void agregarArbitro( Arbitro arbitro){
-        arbitros.add(arbitro);
-    }
+    
+    public List<Arbitro> getArbitros(){ return arbitros; }
+    public void setArbitros(ArrayList<Arbitro> arbitros){ this.arbitros= arbitros; } 
+    public void agregarArbitro( Arbitro arbitro){ arbitros.add(arbitro); }
+
+    public void setSedes(List<Sede> sedes){ this.sedes= sedes; }
+    public List<Sede> getSedes(){ return this.sedes; } 
+    public void agregarSede(Sede sede){ this.sedes.add(sede) } 
 }
-//a
