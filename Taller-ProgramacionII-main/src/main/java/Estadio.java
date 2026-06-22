961e265303254bfee127ab9d5d8e7cd4a6642ab7
diegonat17 <diegonalarcont.17@gmail.com>
2026-06-22 19:50:14 -0300

    Update Estadio.java

--- Taller-ProgramacionII-main/src/main/java/Estadio.java
+++ Taller-ProgramacionII-main/src/main/java/Estadio.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estadio {
     private String nombre;
     private int capacidad;
-    private Sede sede; // Relación con la clase Sede
+    private Sede sede; 
+    private List<Partido> partidos; 
 
     public Estadio(String nombre, int capacidad, Sede sede) {
         this.nombre = nombre;
         this.capacidad = capacidad;
         this.sede = sede;
+        this.partidos= new ArrayList<Partido>();
     }
 
     public String getNombre() { return nombre; }
@@ -17,4 +22,8 @@ public class Estadio {
 
     public Sede getSede() { return sede; }
     public void setSede(Sede sede) { this.sede = sede; }
+
+    public List<Partido> getPartidos(){ return this.partidos; }
+    public void setPartidos(List<Partido> partidos){ this.partidos= partidos; }
+    public void agregarPartido(Partido partido) { this.partidos.add(partido); } 
 }
