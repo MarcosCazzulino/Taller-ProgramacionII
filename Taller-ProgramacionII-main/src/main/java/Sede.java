2b1bb80bd4461eb0c7201b7869eed4ed481ec644
diegonat17 <diegonalarcont.17@gmail.com>
2026-06-22 21:26:07 -0300

    Update Sede.java

--- Taller-ProgramacionII-main/src/main/java/Sede.java
+++ Taller-ProgramacionII-main/src/main/java/Sede.java
@@ -6,22 +6,21 @@ public class Sede {
     private float alturaNivelMar;
     private String clima;
     private String zonaHoraria;
-    private ArrayList<Partido> partidos;
+    private ArrayList<Estadio> estadios;
+    
     public Sede(Pais pais, String ciudad, float alturaNivelMar, String clima, String zonaHoraria) {
-        this.partidos= new ArrayList<>();
+        this.estadios= new ArrayList<Estadio>();
         this.pais = pais;
         this.ciudad = ciudad;
         this.alturaNivelMar = alturaNivelMar;
         this.clima = clima;
         this.zonaHoraria = zonaHoraria;
     }
-    //Metodo para agregar partidos.
-    public void agregarPartidos(Partido partido){
-        partidos.add(partido);
-    }
-    public int cantidadPartidos(){
-        return partidos.size();
-    }
+    
+    public void setEstadios(ArrayList<Estadio> estadios) { this.estadios = estadios; } 
+    public ArrayList<Estadio> getEstadios(){ return this.estadios; }
+    public void agregarEstadio(Estadio estadio){ this.estadios.add(estadio); }
+    //public int cantidadPartidos(){ return partidos.size(); }
 
     public String getCiudad() { return ciudad; }
     public void setCiudad(String ciudad) { this.ciudad = ciudad; }
@@ -34,4 +33,7 @@ public class Sede {
 
     public String getZonaHoraria() { return zonaHoraria; }
     public void setZonaHoraria(String zonaHoraria) { this.zonaHoraria = zonaHoraria; }
+
+    public void setPais(Pais pais) { this.pais= pais; }
+    public Pais getPais() { return this.pais; }
 }
